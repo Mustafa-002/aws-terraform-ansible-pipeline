796b9cbee85a57b45b57452b772e46963cb1bb1f
Mustafa-002 <150596826+Mustafa-002@users.noreply.github.com>
2026-09-08 13:09:37 +0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.80.0"
+      version = "~> 5.0"
     }
   }
 
   backend "s3" {
     bucket = "bucket-for-devops-labs"
     key    = "iac-lab/terraform.tfstate"
-    region = "us-east-1" # Ensure this matches the region where the bucket was created
+    region = "us-east-1"
   }
 }
